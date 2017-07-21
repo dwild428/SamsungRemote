1c7371a5fd520bc129d738a85c541fdbf53164eb
dwild428 <dwild428@gmail.com>
2017-07-21 18:41:23 -0400

    remove pyc, commit tests

--- server/test.py
+++ server/test.py
@@ -1,9 +1,21 @@
 import unittest
 import remote
+import time
 
 class Tester(unittest.TestCase):
     r = remote.SamsungRemote()
-    r.goToChannel(632)
+    r.goToChannel_slow(555)
+    # r.toggleNumbers()
+    # time.sleep(.8)
+    # r.control('KEY_ENTER')  # 6
+    # r.control('KEY_LEFT')
+    # r.control('KEY_LEFT')
+    # r.control('KEY_LEFT')
+    # r.control('KEY_ENTER') #3
+    # r.control('KEY_LEFT')
+    # r.control('KEY_ENTER')
+    # r.control('KEY_UP')
+    # r.control('KEY_ENTER')
 
 if __name__ == "__main__":
     unittest.main()
