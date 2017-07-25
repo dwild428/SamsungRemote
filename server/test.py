a6b8112fec21732d55f51846eac65b1cc8fced7b
dwild428 <dwild428@gmail.com>
2017-07-25 01:35:08 -0400

    unit test

--- server/test.py
+++ server/test.py
@@ -4,30 +4,33 @@ import requests
 
 class Tester(unittest.TestCase):
     SAMSUNG_SERVER_HOSTNAME = 'http://dwild428.hopto.org:81'
-    request = {
-        "directive": {
-            "header": {
-                "namespace": "Alexa.StepSpeaker",
-                "name": "AdjustVolume",
-                "messageId": "c8d53423-b49b-48ee-9181-f50acedf2870",
-                "payloadVersion": "3"
-            },
-            "endpoint": {
-                "scope": {
-                    "type": "BearerToken",
-                    "token": "some-access-token"
-                },
-                "endpointId": "device-001",
-                "cookie": {
 
+    def testrequest(self):
+        request = {
+            "directive": {
+                "header": {
+                    "namespace": "Alexa.StepSpeaker",
+                    "name": "AdjustVolume",
+                    "messageId": "c8d53423-b49b-48ee-9181-f50acedf2870",
+                    "payloadVersion": "3"
+                },
+                "endpoint": {
+                    "scope": {
+                        "type": "BearerToken",
+                        "token": "some-access-token"
+                    },
+                    "endpointId": "device-001",
+                    "cookie": {
+
+                    }
+                },
+                "payload": {
+                    "volumeSteps": -10
                 }
-            },
-            "payload": {
-                "volumeSteps": -10
             }
         }
-    }
-    requests.post('http://dwild428.hopto.org:81', json=request, timeout = 10)
+        resp = requests.post('http://dwild428.hopto.org:81', json=request, timeout = 10)
+        self.assertEquals(resp.code, 200)
 
 if __name__ == "__main__":
     unittest.main()
